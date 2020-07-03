0b78140c7d2a84d0faebbd8b710a349c304efdb5
shdinx <shd.inx@gmail.com>
2020-07-03 23:11:20 +1000

    add mit

--- setup/setup.js
+++ setup/setup.js
@@ -1,4 +1,22 @@
 <script runat="server" language="javascript">
+    //  NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+    // 
+    //  USE YOUR OWN COPY. IT IS EXTREMELY UNWISE TO LOAD CODE FROM SERVERS YOU DO
+    //  NOT CONTROL.
+    // 
+    // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+    // documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+    // the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+    // and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    // 
+    // The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+    // of the Software.
+    // 
+    // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+    // TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+    // THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+    // CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+    // DEALINGS IN THE SOFTWARE.
     Platform.Load("Core", "1.1.5");
     Write('<pre>');
 
