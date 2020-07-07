4e9bd96728695f35c940040f6c9cadf2c102ec0f
shdinx <shd.inx@gmail.com>
2020-07-07 19:09:46 +1000

    final update

--- setup/setup.js
+++ setup/setup.js
@@ -18,7 +18,7 @@
     // CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
     // DEALINGS IN THE SOFTWARE.
     Platform.Load("Core", "1.1.5");
-    var prefix = 'LE';
+    var prefix = 'Email360';
 
     Write('<pre>');
 
